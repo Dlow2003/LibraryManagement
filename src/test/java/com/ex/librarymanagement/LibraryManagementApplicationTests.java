7f91af9abfef185828f7e0c6a75ae9fb212c2be6
DAT099999 <datnvt.21it@vku.udn.vn>
2023-10-02 09:52:09 +0700

    update login

---
+++ src/test/java/com/ex/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
